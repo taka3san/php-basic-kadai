d78173cf79142a0ff905cf5747724ab1e7fedb6d
taka3san <136207759+taka3san@users.noreply.github.com>
2023-09-02 18:05:18 +0900

    課題7

---
+++ kadai_007/associative-arrary-k.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列での出力</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値の代入
+    $Array = ["name"  => "onion", "price" => 200, "weight" => 160];
+
+    // 値の出力
+    print_r($Array);
+    ?>
+  </p>
+
+</body>
+
+</html>
